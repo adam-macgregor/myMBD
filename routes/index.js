dc8f5b210b6ea552916f325a64102586f486297a
adam-macgregor <43255693+adam-macgregor@users.noreply.github.com>
2020-10-13 12:12:07 -0500

    List edit page styled

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const List = require('../models/list')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    const recentlyAddedMovies = await Movie.find({}).sort({ _id: -1 }).limit(3).exec() 
+    const recentlyAddedMovies = await Movie.find({}).sort({ _id: -1 }).limit(10).exec() 
     res.render('index', { recentlyAddedMovies: recentlyAddedMovies })
 })
 
